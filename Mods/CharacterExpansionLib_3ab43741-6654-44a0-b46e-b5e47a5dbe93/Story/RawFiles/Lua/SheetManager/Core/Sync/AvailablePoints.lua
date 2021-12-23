3b898f5dc5de15c0cf2dc57756c743b188ea9fca
LaughingLeader <laughingleader@gmail.com>
2021-12-23 14:28:41 -0600

    Try and workaround available point syncing being before point changes

--- Mods/CharacterExpansionLib_3ab43741-6654-44a0-b46e-b5e47a5dbe93/Story/RawFiles/Lua/SheetManager/Core/Sync/AvailablePoints.lua
+++ Mods/CharacterExpansionLib_3ab43741-6654-44a0-b46e-b5e47a5dbe93/Story/RawFiles/Lua/SheetManager/Core/Sync/AvailablePoints.lua
@@ -57,14 +57,47 @@ if not isClient then
 	RegisterNetListener("CEL_SheetManager_RequestAvailablePoints", function(cmd, payload)
 		local netid = tonumber(payload)
 		if netid then
-			local character = Ext.GetCharacter(netid)
+			local character = GameHelpers.GetCharacter(netid)
 			if character then
+				Timer.Cancel("CEL_RequestSyncAvailablePoints", character.MyGuid)
 				SheetManager.Sync.AvailablePoints(character)
 			end
 		end
 	end)
+
+	RegisterNetListener("CEL_SheetManager_RequestAvailablePointsWithDelay", function(cmd, payload)
+		local netid = tonumber(payload)
+		if netid then
+			local character = GameHelpers.GetCharacter(netid)
+			Timer.StartObjectTimer("CEL_RequestSyncAvailablePoints", character.MyGuid, 500)
+		end
+	end)
+
+	Timer.RegisterListener("CEL_RequestSyncAvailablePoints", function(timerName, uuid)
+		local character = GameHelpers.GetCharacter(uuid)
+		if character then
+			SheetManager.Sync.AvailablePoints(character)
+		end
+	end)
+
+	-- local function BasePointsChanged(character)
+	-- 	character = StringHelpers.GetUUID(character)
+	-- 	local timerName = string.format("CEL_AggregateAvailablePoints%s", character)
+	-- 	Timer.StartOneshot(timerName, 50, function()
+	-- 		SheetManager.Sync.AvailablePoints(character)
+	-- 	end)
+	-- end
+
+	-- Ext.RegisterOsirisListener("CharacterBaseAbilityChanged", 4, "after", BasePointsChanged)
 else
+		---@param characterId UUID|EsvCharacter|NETID|EclCharacter|nil Leave nil to sync points for all players.
+	function SheetManager.Sync.AvailablePointsWithDelay(characterId)
+		local netid = GameHelpers.GetNetID(characterId)
+		Ext.PostMessageToServer("CEL_SheetManager_RequestAvailablePointsWithDelay", tostring(netid))
+	end
+
 	RegisterNetListener("CEL_SheetManager_LoadAvailablePointsForCharacter", function(cmd, payload)
+		Ext.PrintWarning(cmd, payload)
 		local data = Common.JsonParse(payload)
 		if data and data.NetID and data.Points then
 			SheetManager.AvailablePoints[data.NetID] = data.Points
